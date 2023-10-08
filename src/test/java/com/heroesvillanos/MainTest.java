30211c4d7b74f5eee99545ae2fcf2e134e48cdc9
Lucas David Cardozo <lucas.david.car99@gmail.com>
2023-10-08 03:03:56 -0300

    test

---
+++ src/test/java/com/heroesvillanos/MainTest.java
@@ -0,0 +1,13 @@
+package com.heroesvillanos;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    @Disabled
+    public void test(){
+        System.out.println("Hello world!");
+    }
+}
