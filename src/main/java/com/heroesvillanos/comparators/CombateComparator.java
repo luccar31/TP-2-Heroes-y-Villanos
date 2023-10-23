4b212bf4f1ee0a79304ec60fc7711b99c1654c40
lgalante <lgalante@artech-consulting.com>
2023-10-23 19:00:15 -0300

    Comparator funcionando OK

---
+++ src/main/java/com/heroesvillanos/comparators/CombateComparator.java
@@ -0,0 +1,29 @@
+package com.heroesvillanos.comparators;
+
+import java.util.Comparator;
+
+import com.heroesvillanos.dominio.*;
+
+public class CombateComparator implements Comparator<Personaje>{
+	private Caracteristica c = null;
+	
+	public void Comparator(Caracteristica c) {
+		this.c = c;
+	}
+	
+	@SuppressWarnings("static-access")
+	public int compare(Personaje a, Personaje b) {
+		String[] caracteristicas = {"VELOCIDAD", "FUERZA", "RESISTENCIA", "DESTREZA", "VELOCIDAD", "FUERZA", "RESISTENCIA"}; 
+		int restaCaracteristica = -1;
+		for (int i = 0; i < caracteristicas.length; i++) {
+			if(restaCaracteristica == 0) {
+				c = Caracteristica.valueOf(caracteristicas[i]);
+			}
+			if(c.name().equals(caracteristicas[i])) {
+				restaCaracteristica = a.getCaracteristica(c) - b.getCaracteristica(c);
+			}
+		}
+		return restaCaracteristica;
+	}
+
+}
