9b3a9af90bac8ab24c5c662280676cd8c2ee0bb0
thomascu-py <thomascura1998@gmail.com>
2023-11-11 00:01:06 -0300

    Servicio ligas test modificado

--- src/test/java/com/heroesvillanos/ServiciosLigasTest.java
+++ src/test/java/com/heroesvillanos/ServiciosLigasTest.java
@@ -24,6 +24,7 @@ public class ServiciosLigasTest {
 
     private Repositorio<Liga> repositorio;
     private Repositorio<Personaje> repositorioPersonajes;
+
     @InjectMocks
     private ServicioLigas servicioLigas;
 
@@ -32,19 +33,21 @@ public class ServiciosLigasTest {
 
     private final List<Liga> list = new ArrayList<>();
     private final List<Liga> emptyList = new ArrayList<>();
+    private final List<Personaje> personajeEmptyList = new ArrayList<>();
     private final List<Personaje> personajeList = new ArrayList<>();
-    private final List<Personaje> emptyPersonaje = new ArrayList<>();
-    
+
     @BeforeEach
     public void init() {
         tipoCompetidor = TipoCompetidor.HEROE;
         nombreLiga = "dummyNombreLiga";
-        Repositorio<Personaje> repositorioPersonajes = Mockito.mock(Repositorio.class);
+        repositorioPersonajes = Mockito.mock(Repositorio.class);
         repositorio = Mockito.mock(Repositorio.class);
         Persistencia<RegistroLiga, Liga> ligaPersistencia = Mockito.mock(Persistencia.class);
         servicioLigas = new ServicioLigas(repositorio, repositorioPersonajes, ligaPersistencia);
         Liga liga = new Liga(TipoCompetidor.HEROE, "Primera Liga", 1);
         list.add(liga);
+        Personaje personaje = new Personaje(1, "dummyPersonaje", "aka", TipoCompetidor.HEROE, 50, 50, 50, 50);
+        personajeList.add(personaje);
     }
 
     @Test
@@ -54,8 +57,9 @@ public class ServiciosLigasTest {
         Liga outputLiga = servicioLigas.crear(tipoCompetidor, nombreLiga);
         Assertions.assertEquals(list.get(0).getId() + 1, outputLiga.getId());
     }
+
     @Test
-    public void listaCargada_DebeArrojarListaYaCargadaException(){
+    public void listaCargada_DebeArrojarListaYaCargadaException() {
         when(repositorio.listar()).thenReturn(list);
         Assertions.assertThrows(LigasYaCargadasException.class, () -> {
             servicioLigas.cargar();
@@ -63,10 +67,11 @@ public class ServiciosLigasTest {
     }
 
     @Test
-    public void personajesNoCargadosEnCargarLista_DebeArrojarPersonajesNoCargadosException(){
-        when(repositorioPersonajes.listar()).thenReturn(emptyPersonaje);
+    public void personajesNoCargadosEnCargarLista_DebeArrojarPersonajesNoCargadosException() {
+        when(repositorioPersonajes.listar()).thenReturn(personajeEmptyList);
         Assertions.assertThrows(PersonajesNoCargadosException.class, () -> {
             servicioLigas.cargar();
         });
     }
+
 }
